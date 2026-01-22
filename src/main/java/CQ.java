1bce4bc17f04c1f9a6a1f926599f1de1a7df533a
NuowenQ <qiannuowen2004@gmail.com>
2026-01-22 17:02:30 +0800

    Complete level 0 requirements

---
+++ src/main/java/CQ.java
@@ -0,0 +1,24 @@
+public class CQ {
+    String name;
+
+    public CQ() {
+        this.name = "CQ";
+    }
+
+    public void Greet() {
+        String message = "Hello I'm " + this.name;
+        message += "\nWhat can I do for you?";
+        this.ConstructMessage(message);
+    }
+
+    public void Bye() {
+        String message = "Bye. Hope to see you again soon!";
+        this.ConstructMessage(message);
+    }
+
+    public void ConstructMessage(String message) {
+        System.out.println("____________________________________________________________");
+        System.out.println(message);
+        System.out.println("____________________________________________________________");
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-public class Duke {
+public class Main {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +6,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        CQ bot = new CQ();
+        bot.Greet();
+        bot.Bye();
     }
 }
