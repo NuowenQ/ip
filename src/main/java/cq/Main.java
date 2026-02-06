409fd23041a6b4c89620c710294c079e30e57981
NuowenQ <qiannuowen2004@gmail.com>
2026-02-06 14:04:20 +0800

    Complete Level-10

--- src/main/java/cq/Main.java
+++ src/main/java/cq/Main.java
@@ -1,15 +1,32 @@
 package cq;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 /**
  * Entry point for the Cq chatbot application.
  */
-public class Main {
-    /**
-     * Launches the chatbot application.
-     *
-     * @param args command line arguments.
-     */
-    public static void main(String[] args) {
-        ChatbotApp app = new ChatbotApp();
-        app.run();
+public class Main extends Application {
+
+    private Cq bot = new Cq();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            ChatbotApp app = new ChatbotApp(bot);
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setApp(app);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
