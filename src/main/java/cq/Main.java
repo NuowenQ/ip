10a6c86d81c17565f5928ef5c72a3d96ee7bd3c7
NuowenQ <qiannuowen2004@gmail.com>
2026-02-18 00:34:11 +0800

    Implement features to improve UI

--- src/main/java/cq/Main.java
+++ src/main/java/cq/Main.java
@@ -24,6 +24,9 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
+            stage.setMaxWidth(417);
             fxmlLoader.<MainWindow>getController().setApp(app);
             stage.show();
         } catch (IOException e) {
--- src/main/java/cq/ui/MainWindow.java
+++ src/main/java/cq/ui/MainWindow.java
@@ -23,8 +23,8 @@ public class MainWindow extends AnchorPane {
 
     private ChatbotApp app;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/User.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/Normal.png"));
 
     @FXML
     public void initialize() {
